af80c4dab8359711482fdd8ffe0f9b435a4d77b4
WIN-11 <2382011@unai.edu>
2024-11-12 10:14:20 +0700

    Hansond 8

--- Main.java
+++ Main.java
@@ -1,13 +1,18 @@
+import Config.Database;
 import Services.TodoListService;
 import Services.TodoListServicesImpl;
 import Views.TodoListTerminalView;
 import Views.TodoListView;
 import repositories.TodoListRepository;
+import repositories.TodoListRepositoryDbImpl;
 import repositories.TodoListRepositoryImpl;
 
 public class Main {
     public static void main(String[] args) {
-        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        Database database = new Database("databaseku", "root", "", "localhost", "3306");
+        database.setup();
+
+        TodoListRepository todoListRepository = new TodoListRepositoryDbImpl(database);
         TodoListService todoListService = new TodoListServicesImpl(todoListRepository);
         TodoListView todoListView = new TodoListTerminalView(todoListService);
         todoListView.run();
